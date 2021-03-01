6bd282c567d7de995e11edaa293965d62fed2740
brandondiaz <brandon.diaz@bowpi.com>
2021-02-28 21:42:48 -0600

    Changes Se agregó en CalculoIva, que elija cuantos productos desea agregar al inicio y a CalculoIva2 que le pregunte al usuario si deesea agregar mas productos

--- src/programacionB/CalculoIva2.java
+++ src/programacionB/CalculoIva2.java
@@ -2,40 +2,38 @@ package programacionB;
 
 import java.util.Scanner;
 
-public class CalculoIva {
+public class CalculoIva2 {
     public static void main(String[] args) {
         double precio, iva, importe, total = 0, cantidad = 0;
-        int productos;
-        int i = 0;
+        String choise;
         String nombreProducto;
 
         Scanner scanner = new Scanner(System.in);
 
         try{
-            System.out.println("Ingrese la cantidad de productos que desea comprar");
-            productos = scanner.nextInt();
 
-            while(i < productos) {
+            do {
                 System.out.print("Ingrese el nombre del producto: ");
                 nombreProducto = scanner.next();
                 System.out.print("Ingrese el precio del producto: ");
                 precio = scanner.nextDouble();
                 System.out.print("Ingrese la cantidad de producto a comprar: ");
                 //Convertira lo que el usuario ingrese a double, debido a que se capturó lo que ingresó por String
-                cantidad = Double.parseDouble(scanner.next());
 
+                cantidad = Double.parseDouble(scanner.next());
                 importe = precio * cantidad;
                 iva = importe * 0.12;
-                total = total+importe+iva;
-                i++;
+                total = total +importe + iva;
 
-            }
+                System.out.println("Presione S para agregar más productos o N para terminar");
+                choise = scanner.next();
+
+            }while (choise.equals("S") | choise.equals("s"));
 
             System.out.println(total);
 
         }catch (Exception error){
-            System.out.println("Debe ingresar un dato válido " + error);
+            System.out.println("Ingresó un dato incorrecto.");
         }
     }
-
 }
